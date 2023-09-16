8f4b40db24e73557f3ac1f78a7d249ae5f82a9df
Marcos Ernesto Planos Valenzuela <87524702+Dantalian5@users.noreply.github.com>
2023-09-16 19:38:10 +0200

    fix script

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,10 +1,17 @@
 const formInput = document.getElementById("emailInput");
 const formSubmit = document.getElementById("submitButton");
 const form = document.getElementById("myForm");
+let formError = document.getElementById("formError");
 
 formSubmit.addEventListener("click", (event) => {
 	if (!formInput.validity.valid) {
 		form.classList.add("js-error");
+		if (formInput.value == "") {
+			formError.innerText =
+				"Whoops! It looks like you forgot to add your email";
+		} else {
+			formError.innerText = "Please provide a valid email address";
+		}
 	} else {
 		form.classList.remove("js-error");
 	}
