89c8c3f8f952b0484097849798dc3531ef55fc88
Marcos Ernesto Planos Valenzuela <87524702+Dantalian5@users.noreply.github.com>
2023-09-16 19:16:46 +0200

    add script & fix index

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,10 +1,11 @@
-let formInput = document.getElementById("emailInput");
-let formSubmit = document.getElementById("submitButton");
+const formInput = document.getElementById("emailInput");
+const formSubmit = document.getElementById("submitButton");
+const form = document.getElementById("myForm");
 
 formSubmit.addEventListener("click", (event) => {
 	if (!formInput.validity.valid) {
-		formInput.classList.add("js-error");
+		form.classList.add("js-error");
 	} else {
-		formInput.classList.remove("js-error");
+		form.classList.remove("js-error");
 	}
 });
